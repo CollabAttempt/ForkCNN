911884c89c2267967e0c09250f2b2a09c316e3fe
usmancheema89 <48430251+usmancheema89@users.noreply.github.com>
2020-06-10 16:03:14 +0900

    WIP on ECM Embedding

--- myECMSingleModalPredictLoop.py
+++ myECMSingleModalPredictLoop.py
@@ -1,9 +1,9 @@
 from tensorflow.keras import models, Model, Sequential, utils, Input
+from myUtils.ECM_Classifier_Block import classifier_ECM
 # from myUtils.my_Model_Utils import my_embd_loss
 
 def my_embd_loss(y_actual,y_pred):
     return y_pred
-
 
 def extract_classifier(main_model, starting_layer_ix, ending_layer_ix):
     # create an empty model
@@ -16,24 +16,33 @@ def extract_classifier(main_model, starting_layer_ix, ending_layer_ix):
     new_model = Model(input_shape,x)
     return new_model
 
+def create_classifier(main_model):
+    input_shape = main_model.get_layer(name = 'conv' + str(4) + "_" + str(1) + "_1x1_reduce_" + 'clasify').output.shape
+    output_tuple = main_model.outputs[0].shape[1]
+    input_tuple = (input_shape[1], input_shape[2], input_shape[3])
+    class_model = classifier_ECM(input_tuple,output_tuple)
+    
+    for c_layer in class_model.layers:
+        for m_layer in main_model.layers:
+            c_name = c_layer.name
+            if m_layer.name == c_name:
+                print(c_name)
 
+            
 
-model_path = r'E:\Work\Multi Modal Face Recognition\Output\Models\myecm_SejongDB_2_Vis-VisIr-_0_na_3'
-base_model = models.load_model(model_path, custom_objects={'my_embd_loss': my_embd_loss}, compile= False )
+model_path = r'E:\myecm_I2BVSD_2_Vis-The-_0_na_3'
+base_model = models.load_model(model_path, compile= False)
 print(base_model.summary())
-base_inputs = base_model.inputs
-base_outputs = base_model.outputs
-utils.plot_model(base_model, 'Base_model.png', show_shapes=True)
+
+
+## get average + 1 layer data
+name = 'conv' + str(4) + "_" + str(1) + "_1x1_reduce_" + 'clasify'
 for i in range(len(base_model.layers)):
-    name = 'conv' + str(4) + "_" + str(1) + "_1x1_reduce_" + 'clasify'
     if name == base_model.layers[i].name:
-        print('average:', base_model.layers[i].name, ' ', i)
-        print(base_model.layers[i].input)
+        print('First Layer:', base_model.layers[i].name, ' ', i)
 
-baseavg_layer = base_model.get_layer(name='average')
-print(baseavg_layer.input)
-print(baseavg_layer.output)
 
+## Create embd_model
 embd_in = base_model.inputs[0]
 embd_out = base_model.get_layer(name = 'activation_35')
 embd_model = Model(embd_in,embd_out.output)
@@ -41,19 +50,16 @@ embd_model.compile(optimizer='SGD', loss = my_embd_loss)
 print('EMBD inputs', embd_model.inputs)
 print('EMBD outputs',embd_model.outputs)
 
-class_model = extract_classifier(base_model, 257, len(base_model.layers))
-class_model.compile(optimizer='SGD', loss = my_embd_loss)
-# utils.plot_model(class_model, 'Class_model.png', show_shapes=True)
-print('Class inputs', class_model.inputs)
-print('Class outputs', class_model.outputs)
 
+class_model = create_classifier(base_model)
+# class_model.compile(optimizer='SGD', loss = my_embd_loss)
+# print('Class inputs', class_model.inputs)
+# print('Class outputs', class_model.outputs)
+
+
+# # utils.plot_model(class_model, 'Class_model.png', show_shapes=True)
 # class_name = 'conv' + str(4) + "_" + str(1) + "_1x1_reduce_" + 'clasify'
 # class_in = base_model.get_layer(name = class_name)    
 # class_out = base_outputs[0]
 # class_model = Model(class_in.input,class_out)
 # class_model.compile(optimizer='SGD', loss = my_embd_loss)
-
-
-# for m_output in m_outputs:
-#     print(m_output)
-#     print(m_output.shape) 
---
+++ myUtils/ECM_Classifier_Block.py
@@ -0,0 +1,140 @@
+
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, MaxPooling2D, AveragePooling2D, \
+    GlobalAveragePooling2D, Reshape, Add,  multiply, Dense, Average
+from tensorflow.keras.models import Model
+# from tensorflow.keras.utils import to_categorical
+# from tensorflow import keras
+
+def senet_se_block(input_tensor, stage, block, compress_rate=16, bias=False, name=None):
+    conv1_down_name = 'conv' + str(stage) + "_" + str(block) + "_1x1_down_" + name
+    conv1_up_name = 'conv' + str(stage) + "_" + str(block) + "_1x1_up_" + name
+
+    num_channels = int(input_tensor.shape[-1])
+    bottle_neck = int(num_channels // compress_rate)
+
+    se = GlobalAveragePooling2D()(input_tensor)
+    se = Reshape((1, 1, num_channels))(se)
+    se = Conv2D(bottle_neck, (1, 1), use_bias=bias,
+                name=conv1_down_name)(se)
+    se = Activation('relu')(se)
+    se = Conv2D(num_channels, (1, 1), use_bias=bias,
+                name=conv1_up_name)(se)
+    se = Activation('sigmoid')(se)
+
+    x = input_tensor
+    x = multiply([x, se])
+    return x
+
+def senet_conv_block(input_tensor, kernel_size, filters,
+                     stage, block, bias=False, strides=(2, 2), name=None):
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    bn_eps = 0.0001
+
+    conv1_reduce_name = 'conv' + str(stage) + "_" + str(block) + "_1x1_reduce_" + name
+    conv1_increase_name = 'conv' + str(stage) + "_" + str(
+        block) + "_1x1_increase_" + name
+    conv1_proj_name = 'conv' + str(stage) + "_" + str(block) + "_1x1_proj_" + name
+    conv3_name = 'conv' + str(stage) + "_" + str(block) + "_3x3_" + name
+
+    x = Conv2D(filters1, (1, 1), use_bias=bias, strides=strides,
+               name=conv1_reduce_name)(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=conv1_reduce_name + "/bn", epsilon=bn_eps)(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size, padding='same', use_bias=bias,
+               name=conv3_name)(x)
+    x = BatchNormalization(axis=bn_axis, name=conv3_name + "/bn", epsilon=bn_eps)(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv1_increase_name, use_bias=bias)(x)
+    x = BatchNormalization(axis=bn_axis, name=conv1_increase_name + "/bn", epsilon=bn_eps)(x)
+
+    se = senet_se_block(x, stage=stage, block=block, bias=True, name=name)
+
+    shortcut = Conv2D(filters3, (1, 1), use_bias=bias, strides=strides,
+                      name=conv1_proj_name)(input_tensor)
+    shortcut = BatchNormalization(axis=bn_axis,
+                                  name=conv1_proj_name + "/bn", epsilon=bn_eps)(shortcut)
+
+    m = Add()([se, shortcut])
+    m = Activation('relu')(m)
+    return m
+
+def senet_identity_block(input_tensor, kernel_size,
+                         filters, stage, block, bias=False, name=None):
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    bn_eps = 0.0001
+
+    conv1_reduce_name = 'conv' + str(stage) + "_" + str(block) + "_1x1_reduce" + name
+    conv1_increase_name = 'conv' + str(stage) + "_" + str(
+        block) + "_1x1_increase" + name
+    conv3_name = 'conv' + str(stage) + "_" + str(block) + "_3x3_" + name
+
+    x = Conv2D(filters1, (1, 1), use_bias=bias,
+               name=conv1_reduce_name)(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=conv1_reduce_name + "/bn", epsilon=bn_eps)(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size, padding='same', use_bias=bias,
+               name=conv3_name)(x)
+    x = BatchNormalization(axis=bn_axis, name=conv3_name + "/bn", epsilon=bn_eps)(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv1_increase_name, use_bias=bias)(x)
+    x = BatchNormalization(axis=bn_axis, name=conv1_increase_name + "/bn", epsilon=bn_eps)(x)
+
+    se = senet_se_block(x, stage=stage, block=block, bias=True, name=name)
+
+    m = Add()([se, input_tensor])
+    m = Activation('relu')(m)
+
+    return m
+
+def clasi_block(x, name):
+    filter3 = K.int_shape(x)[3]
+    x = senet_conv_block(x, 3, [256, 256, filter3], stage=4, block=1, name=name)
+    x = senet_identity_block(x, 3, [256, 256, filter3], stage=4, block=2, name=name)
+    x = senet_identity_block(x, 3, [256, 256, filter3], stage=4, block=3, name=name)
+
+    filter3 = K.int_shape(x)[3]
+    x = senet_identity_block(x, 3, [256, 256, filter3], stage=4, block=4, name=name)
+    x = senet_identity_block(x, 3, [256, 256, filter3], stage=4, block=5, name=name)
+    x = senet_identity_block(x, 3, [256, 256, filter3], stage=4, block=6, name=name)
+
+    x = senet_conv_block(x, 3, [512, 512, filter3], stage=5, block=1, name=name)
+    x = senet_identity_block(x, 3, [512, 512, filter3], stage=5, block=2, name=name)
+    x = senet_identity_block(x, 3, [512, 512, filter3], stage=5, block=3, name=name)
+    size = x.shape[1]
+    x = AveragePooling2D((size - 1, size - 1), name='avg_pool')(x)
+    return x
+
+
+def classifier_ECM(input_shape, classes):
+    input_tensor = Input(shape=input_shape)
+    bn_axis = 3
+    bn_eps = 0.0001
+    model_name = 'myECM'
+
+    layers = clasi_block(input_tensor, 'clasify')
+
+    output = Flatten()(output)
+    output = Dense(classes, activation='softmax', name='classifier')(output)
+
+    # Create model.
+    model = Model(input_tensor, output, name = model_name)
+    print(model.summary())
+
+    return model
+
+    
